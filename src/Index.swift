0683fd48b4b243a2a3f91b95b0412249f816df14
Dirk Gadsden <dirk@esherido.com>
2015-07-12 16:20:37 -0700

    Set up basic writing of index file

--- src/Index.swift
+++ src/Index.swift
@@ -2,8 +2,32 @@ import Foundation
 
 class Index {
 
+  
+  private let formatter: NSDateFormatter = NSDateFormatter()
+
+  init() {
+    formatter.dateFormat = "yyyy-mm-dd'T'HH-mm-ss'Z'"
+
+  }
+
+
   func packPackages(packages: [Package]) {
-    return
+    let path = getOutputName()
+
+    if !fileManager.createFileAtPath(path, contents: nil, attributes: nil) {
+      printAndExit("Failed to create index file '\(path)'")
+    }
+
+    let file = NSFileHandle(forWritingAtPath: getOutputName())!
+
+    let header = "Roost Index Version 1\n"
+    file.writeData(header.dataUsingEncoding(NSUTF8StringEncoding)!)
+  }
+
+  func getOutputName() -> String {
+    let date = formatter.stringFromDate(NSDate())
+
+    return "Index-\(date).bin"
   }
 
 }
