7a1acee970ef4a2cdbfa7a33d1ab352885bf1e4f
Dirk Gadsden <dirk@esherido.com>
2015-07-14 20:26:53 -0700

    Implement index packing

--- src/Index.swift
+++ src/Index.swift
@@ -1,15 +1,13 @@
 import Foundation
+import MessagePack
 
 class Index {
-
   
   private let formatter: NSDateFormatter = NSDateFormatter()
 
   init() {
     formatter.dateFormat = "yyyy-mm-dd'T'HH-mm-ss'Z'"
-
   }
-
 
   func packPackages(packages: [Package]) {
     let path = getOutputName()
@@ -22,9 +20,29 @@ class Index {
 
     let header = "Roost Index Version 1\n"
     file.writeData(header.dataUsingEncoding(NSUTF8StringEncoding)!)
+
+    var dictionary = [MessagePackValue : MessagePackValue]()
+
+    for package in packages {
+      let key = MessagePackValue.String(package.name)
+      let value = packedPackage(package)
+
+      dictionary[key] = value
+    }
+
+    let packed = MessagePack.pack(.Map(dictionary))
+
+    file.writeData(packed)
   }
 
-  func getOutputName() -> String {
+  private func packedPackage(package: Package) -> MessagePackValue {
+    return MessagePackValue.Map([
+      .String("name")    : .String(package.name),
+      .String("version") : .String(package.version.description)
+    ])
+  }
+
+  private func getOutputName() -> String {
     let date = formatter.stringFromDate(NSDate())
 
     return "Index-\(date).bin"
